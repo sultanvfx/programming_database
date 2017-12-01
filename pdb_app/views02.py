3eef999c92d52367d255bc7ae12fbaeec6eda16a
sultanvfx <sultanvfx@gmail.com>
2017-12-01 18:43:55 +0530

    Get All data from Database and Show it using For-loop in html.

--- pdb_app/views02.py
+++ pdb_app/views02.py
@@ -13,5 +13,5 @@ def index(request):
     # return HttpResponse("<h1>Programming Database</h1>")  #way #1 to display html
     # +++++++++++++++++++++++++++
     template = loader.get_template("index.html")  # way #2 to display html
-    context = {'all_items': Item.objects.all()}  # data to send into the index.html file ie. the template. We sending the all Item "objects" to html.
+    context = {'java': Item.objects.get(id=2)}  # data to send into the index.html file ie. the template. We sending the "object" to html.
     return HttpResponse(template.render(context, request))
